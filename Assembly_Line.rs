ec7bc60d4521d7d7c923a98c6d0e5f0b75ec33a7
옥지원 <77279316+JiWonOck@users.noreply.github.com>
2022-03-08 18:17:32 +0900

    Create Assembly_Line.rs

---
+++ Assembly_Line.rs
@@ -0,0 +1,17 @@
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    success_rate(speed) * 221.0 * (speed as f64) // <------ factor order of that line matters!
+}
+
+fn success_rate(speed: u8) -> f64 {
+    match speed {
+        0      => 0.0,
+        1..=4  => 1.0,
+        5..=8  => 0.9,
+        9..=10 => 0.77,
+        _ => panic!("speed must be between 0..=10, but received {} instead.", speed),
+    }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
